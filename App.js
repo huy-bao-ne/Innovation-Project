ba23e696d0243abb181f9697d9722421d13ebe71
huy-bao-ne <144243646+huy-bao-ne@users.noreply.github.com>
2025-03-21 08:48:46 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from "./component/homepage";
-import GradingPage from "./component/GradingPage";
-import Login from "./component/login";
-import Navbar from "./component/Navbar";
+import Homepage from "./component/homepage.js";
+import GradingPage from "./component/GradingPage.js";
+import Login from "./component/login.js";
+import Student from "./component/Student.js";
+import Teacher from "./component/Teacher.js";
+
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/grading" element={<GradingPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/student-dashboard" element={<Student />} />
+        <Route path="/teacher-dashboard" element={<Teacher />} />
       </Routes>
     </Router>
   );
