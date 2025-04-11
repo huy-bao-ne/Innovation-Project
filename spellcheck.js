295766a78b0b7de14034db192bbe9cf1d255295c
huy-bao-ne <144243646+huy-bao-ne@users.noreply.github.com>
2025-04-11 10:36:48 +0700

    Delete spellcheck.js

--- spellcheck.js
+++
@@ -1,5 +0,0 @@
-export const checkSpelling = (text) => {
-  const dictionary = ["history", "revolution", "freedom", "democracy", "government", "independence"];
-  const words = text.toLowerCase().split(/\s+/);
-  return words.filter(word => !dictionary.includes(word) && word.length > 3);
-};
